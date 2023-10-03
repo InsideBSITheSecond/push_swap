727e1e3796fadda235eea007d8fb3b79c209ed25
InsideBSI <insidebsiprod@gmail.com>
2023-10-03 18:35:17 +0200

    should pass tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,8 @@
 set -e
 
 make -s SLEEP=0 OPSONLY=1 re
-#cmd=(./push_swap 123 234 345 456 567 678 789)
-cmd=(./push_swap dqzfabg)
+cmd=(./push_swap 123 234 345 456 567 678 789)
+#cmd=(./push_swap dqzfabg)
 
 if "${cmd[@]}" | grep -q 'UwU'; then
    echo "program execution ended"
