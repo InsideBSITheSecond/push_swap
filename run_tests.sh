b4bbd46a30d4a8a3c2ca9caa2b887d941832ba8b
InsideBSI <insidebsiprod@gmail.com>
2023-10-04 23:48:27 +0200

    hmmm

--- run_tests.sh
+++ run_tests.sh
@@ -4,8 +4,6 @@ make -s SLEEP=0 OPSONLY=1 re
 cmd=(./push_swap 123 234 345 456 567 678 789)
 #cmd=(./push_swap dqzfabg)
 
-if "${cmd[@]}" | grep -q 'UwU'; then
+if "${cmd[@]}" | grep -q 'pa'; then
    echo "program execution ended"
 fi
-
-"${cmd[@]}"
