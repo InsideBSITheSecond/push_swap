b0e2f7b9abf7293fa3d02aa577f053d4e88b7752
InsideBSI <insidebsiprod@gmail.com>
2023-10-06 22:35:31 +0200

    libft update

--- run_tests.sh
+++ run_tests.sh
@@ -4,11 +4,11 @@ make -s dep
 make -j4 push_swap
 make -j4 SILENT=1 PRINTSTACK=1 checker
 
-#printf "\ntest 1: (0->250)*25\n"
-#RAND=$(python -c 'import random as r; print(*r.sample(range(0, 250), 25), sep=" ");')
-#echo ${RAND}
-#./push_swap ${RAND} | ./checker ${RAND}
-#printf "\n"
+printf "\ntest 1: (0->250)*25\n"
+RAND=$(python -c 'import random as r; print(*r.sample(range(0, 250), 25), sep=" ");')
+echo ${RAND}
+./push_swap ${RAND} | ./checker ${RAND}
+printf "\n"
 
 printf "\ntest 2: (-125->125)*25\n"
 RAND=$(python -c 'import random as r; print(*r.sample(range(-125, 125), 25), sep=" ");')
@@ -16,5 +16,3 @@ echo ${RAND}
 ./push_swap ${RAND} | ./checker ${RAND}
 printf "\n"
 
-#RAND=$(python -c 'import random as r; print(*r.sample(range(-250, 0), 25), sep=" ");')
-#./push_swap ${RAND}
