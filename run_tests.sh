460b8525231c87d5bc2a0f97b07fc7c7c2b1ba79
InsideBSI <insidebsiprod@gmail.com>
2023-10-07 19:20:29 +0200

    tests update

--- run_tests.sh
+++ run_tests.sh
@@ -1,49 +1,19 @@
 #!/bin/bash
+
+perform_test() {
+    local python_command="import random as r; print(*r.sample(range($1, $2), $3), sep=\" \");"
+    local printf_string="\ntest 1: ($1->$2)*$3\n"
+
+	printf "==================="
+    printf "$printf_string"
+    RAND=$(python -c "$python_command")
+    ./push_swap ${RAND} | wc -l
+    time ./push_swap ${RAND} | ./checker ${RAND}
+	printf "==================="
+}
+
 make -s fclean
 make -s dep
 make -j4 push_swap
 make -j4 SILENT=1 PRINTSTACK=0 checker
-
-printf "\ntest 1: (0->250)*25\n"
-RAND=$(python -c 'import random as r; print(*r.sample(range(0, 250), 25), sep=" ");')
-#echo ${RAND}
-time ./push_swap ${RAND} | ./checker ${RAND}
-
-printf "============="
-
-printf "\ntest 2: (-125->125)*25\n"
-RAND=$(python -c 'import random as r; print(*r.sample(range(-125, 125), 25), sep=" ");')
-#echo ${RAND}
-time ./push_swap ${RAND} | ./checker ${RAND}
-
-printf "============="
-
-
-printf "\ntest 3: (-1250->1250)*250\n"
-RAND=$(python -c 'import random as r; print(*r.sample(range(-1250, 1250), 250), sep=" ");')
-#echo ${RAND}
-time ./push_swap ${RAND} | ./checker ${RAND}
-
-printf "============="
-
-printf "\ntest 4: (-12500->12500)*2500\n"
-RAND=$(python -c 'import random as r; print(*r.sample(range(-12500, 12500), 2500), sep=" ");')
-#echo ${RAND}
-time ./push_swap ${RAND} | ./checker ${RAND}
-
-printf "============="
-
-printf "\ntest 5: (-125000->125000)*2500\n"
-RAND=$(python -c 'import random as r; print(*r.sample(range(-125000, 125000), 2500), sep=" ");')
-#echo ${RAND}
-time ./push_swap ${RAND} | ./checker ${RAND}
-
-printf "============="
-
-printf "\ntest 6: already sorted\n"
-time ./push_swap 1 2 3 4 5 6 7 8 9 | ./checker 1 2 3 4 5 6 7 8 9
-printf "============="
-
-printf "\ntest 6: duplicates\n"
-time ./push_swap 1 2 3 4 5 6 7 8 9 3 | ./checker 1 2 3 4 5 6 7 8 9 3
-printf "============="
+perform_test "$1" "$2" "$3"
